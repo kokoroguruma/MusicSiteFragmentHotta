e9a122e032568be384e92af2610221e29515d180
Kokoroguruma <kokoroguruma@hotmail.com>
2018-03-23 17:57:21 +0900

    PlyaRightCommentList UIデザイン完成。

--- app/src/main/java/com/kokoroguruma/musicsitefragmenthotta/PlayRightCommentListFragment.java
+++ app/src/main/java/com/kokoroguruma/musicsitefragmenthotta/PlayRightCommentListFragment.java
@@ -3,9 +3,15 @@ package com.kokoroguruma.musicsitefragmenthotta;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -27,5 +33,67 @@ public class PlayRightCommentListFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_play_right_comment_list, container, false);
     }
+
+    @Override
+    public void onResume() {
+
+        this.setComponent();
+
+        super.onResume();
+    }
+
+
+    // ここからコンポーネント実装
+
+    private void setComponent() {
+        this.setClickPlayRightCommentAddButton();
+        this.setList();
+    }
+
+    private void setClickPlayRightCommentAddButton() {
+        Button button = getView().findViewById(R.id.playRightCommentAddButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("PlayRightCom-Fragment: ", "setClickPlay-AddButton: " + v);
+                // TODO: コメント追加
+                updateList();
+            }
+        });
+    }
+
+    private void setList() {
+        ListView listView = getView().findViewById(R.id.playRightList);
+
+
+        // TODO: Listデータ引き出し
+        ArrayList<String> arrayList = new ArrayList<String>();
+        for (int i=0; i<10; i++) {
+            String string = "Comment: " + i;
+            arrayList.add(string);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+
+    }
+
+    private void updateList() {
+        // TODO: List更新
+
+        ListView listView = getView().findViewById(R.id.playRightList);
+        ArrayAdapter<String> adapter = (ArrayAdapter<String>) listView.getAdapter();
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+        for (int i=0; i<10; i++) {
+            String string = "Comment2: " + i;
+            arrayList.add(string);
+        }
+
+        adapter.clear();
+        adapter.addAll(arrayList);
+        adapter.notifyDataSetChanged();
+    }
+
 
 }
